b9eb517fa3c1a11ea40a34f4544d561d026c8183
Sherief Farid <sherief@farid.cloud>
2019-02-10 03:58:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz (n)
-  if n % 3 == 0 && n% 5 ==0
+  if (n % 3 == 0) && (n % 5 ==0)
     message = "fizzbuzz"
 #  elsif n % 5 == 0
 #    message = "Buzz"

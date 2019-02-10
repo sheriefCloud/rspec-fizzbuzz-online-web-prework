ead2170c1164c9953f309902e52258b155dafc97
Sherief Farid <sherief@farid.cloud>
2019-02-10 03:56:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,15 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz (n)
-
+  if n % 3 == 0 && n% 5 ==0 
+    message = "fizzbuzz"
+  elsif n % 5 == 0
+    message = "Buzz"
+  elsif n % 3 == 0
+    message = "Fizz"
+  else
+    return nil
+return message
+    
+    
 end

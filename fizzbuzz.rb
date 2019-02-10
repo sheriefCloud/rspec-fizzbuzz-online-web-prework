b15f221aada1874ab5db43b1c9e5bb87c42f3f33
Sherief Farid <sherief@farid.cloud>
2019-02-10 04:00:22 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz (n)
+  if ((n % 3 == 0) && (n % 5 ==0))
+    message = "FizzBuzz"
+  elsif n % 5 == 0
+    message = "Buzz"
+  elsif n % 3 == 0
+    message = "Fizz"
+  else
+    return nil
+end
+return message
+
+
+
+end
